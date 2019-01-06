e7733a841157fc86b16107b6be5f60e727c02cb6
Stephen Wright <scw248@gmail.com>
2019-01-06 00:21:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,4 @@
+require "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
@@ -8,6 +9,7 @@ def key_for_min_value(name_hash)
     leastArray = []
     least = amount
     if amount < least
+      binding.pry
       leastArray << item
       return leastArray[-1]
     end
