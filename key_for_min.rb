2948e238d32efd9c25cc3e079b5ea7b1b7bab4fb
Stephen Wright <scw248@gmail.com>
2019-01-06 00:20:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   return nil if name_hash.length == 0
   
   name_hash.each do |item, amount|
+    leastArray = []
     least = amount
     if amount < least
-      least = amount 
-      return item
+      leastArray << item
+      return leastArray[-1]
     end
   end
 end
