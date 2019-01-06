f80318ae1a02c1b502438ccff56657b756bbce96
Stephen Wright <scw248@gmail.com>
2019-01-06 00:51:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.length == 0
-  least = 1000
+  least = nil
   least_item = nil
   
   name_hash.each do |item, amount|
-    if amount < least
+    if least == nil || amount < least
       least = amount
       least_item = item
     end
